e04b3bbb459f28f98034f14c244e69b87c67ade2
mudit <mudit.shukla_cs19@gla.ac.in>
2020-08-30 14:18:46 +0530

    crating player variable of Person class defined in game.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from classes.game import Person,bcolors
 
+magic = [{"name": "Fire","cost": 10,"dmg": 60},
+         {"name": "thunder","cost": 10,"dmg": 60},
+         {"name": "blizzard","cost": 10,"dmg": 60}]
+
+player = Person(450,65, 60, 34, magic)
